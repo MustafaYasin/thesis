671430e13d8349b3b5519c670dbae72bc03a22bb
Mustafa Yasin <myasin@adobe.com>
2022-10-12 11:55:39 +0200

    setting up sqlalchemy

--- app/backend/storing_data_from_csv.py
+++ app/backend/storing_data_from_csv.py
@@ -5,13 +5,12 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine('sqlite:///profiles_dbase.sqlite', echo=False)
+engine = create_engine('sqlite:///profiles.db', echo=False)
 
 Base = declarative_base()
 
 class Profiles(Base):
     __tablename__ = 'profiles'
-    __table_args__ = {'autoload': True}
 
     profile_id = Column(Integer, primary_key=True)
     username = Column(String)
@@ -24,12 +23,15 @@ class Profiles(Base):
     star_time = Column(String)
 
     def __repr__(self):
-        return format(self.username, self.name, self.email, self.repo_count, self.company, self.avatar_url, self.hireable, self.star_time)
+        return '''<Profiles(profile_id='{0}', username='{1}', name='{2}', email='{3}', repo_count='{4}', company='{5}', avatar_url='{6}', hireable='{7}', star_time='{8}')>'''.format(self.profile_id, self.username, self.name, self.email, self.repo_count, self.company, self.avatar_url, self.hireable, self.star_time)
 
 Base.metadata.create_all(engine)
 
-file_name = 'profiles.csv'
-    
+
+
+file_name = 'csv_folder/pytorch_pytorch.csv'
+print(file_name)
+
 df = pandas.read_csv(file_name)
 
 df.to_sql(con = engine, name=Profiles.__tablename__, if_exists='append', index=False)
@@ -37,6 +39,8 @@ df.to_sql(con = engine, name=Profiles.__tablename__, if_exists='append', index=F
 session = sessionmaker()
 session.configure(bind=engine)
 s = session()
+
+
 
 results = s.query(Profiles).limit(10).all()
 for r in results:

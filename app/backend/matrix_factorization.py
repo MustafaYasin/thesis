6004a1560a0cfde72bf9a4e6b44fe1795dfd2ce5
Mustafa Yasin <myasin@adobe.com>
2022-10-19 18:53:12 +0200

    preparing dataset

--- app/backend/matrix_factorization.py
+++ app/backend/matrix_factorization.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
 
-ratings_list = [i.strip().split("::") for i in open('/Users/myasin/Documents/thesis/app/backend/csv_folder/pytorch_pytorch.csv', 'r',).readlines()]
-
-ratings_df = pd.DataFrame(ratings_list, columns = ['repo_count', 'company', 'hireable'], dtype = int)
 
 
-ratings_df.head()
+
+df = pd.read_csv('/Users/myasin/Documents/thesis/app/backend/csv_folder/pytorch_pytorch.csv')
+df.head()

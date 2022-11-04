ac54f46c8e074492404a13ce0e807a23b60fb5cb
Mustafa Yasin <myasin@adobe.com>
2022-11-04 18:02:55 +0100

    some issue happening with csv files

--- project/backend/recommender_system/mock_recommendation.py
+++ project/backend/recommender_system/mock_recommendation.py
@@ -12,13 +12,13 @@ result = list(real_profile_db.find({}, {'_id': 0}))
 pprint(result[0])
 email_to_recommendation = dict()
 
-max_repo_count = max(result, key=lambda x: x['repo_count'])['repo_count']
+max_repo_count = max(result, key=lambda x: x['repository_count'])['repository_count']
 
 
 for user in result:
     email = user['email']
-    repo_count = user['repo_count']
+    repository_count = user['repository_count']
     hireable = user['hireable']
-    recommendation = (repo_count / max_repo_count) * hireable * 100
+    recommendation = (repository_count / max_repo_count) * hireable * 100
     real_profile_db.update_one({'email': email}, {'$set': {'recommendations': recommendation}})
 

24a58cd23d4fbfb7d6e81c9b960eba6ffc357702
rhofmann <r.hofmann2711@gmail.com>
2022-11-04 18:08:32 +0100

    FE updates

---
+++ project/frontend/src/pages/HomePage.js
@@ -0,0 +1,31 @@
+import Card from "../components/ui/Card";
+import { Link } from "react-router-dom";
+import classes from "./HomePage.module.css";
+
+function CompareFavoritesPage() {
+  return (
+    <>
+      <h1>HomePage</h1>
+      <div className={classes.dataScientist}>
+        <Card>
+          <div>
+            <h3>Looking for Data Scientist?</h3>
+            <div className={classes.image}>
+              <img
+                src="https://www.sudeep.co/images/post_images/2018-02-09-Understanding-the-Data-Science-Lifecycle/chart.png"
+                alt=""
+              />
+              <div className={classes.link}>
+                <Link to="/recommend">
+                  <button> Click Here</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
+}
+
+export default CompareFavoritesPage;
--- project/frontend/src/pages/RecommendedProfilesPage.js
+++ project/frontend/src/pages/RecommendedProfilesPage.js
@@ -11,52 +11,7 @@ import Backdrop from "../components/ui/Backdrop";
 import ProfileList from "../components/profiles/ProfileList";
 import Modal from "../components/ui/Modal";
 import { useEffect } from "react";
-
-const DUMMY_DATA = [
-  {
-    email: "dsoller@outlook.com",
-    avatar:
-      "https://avatars.githubusercontent.com/u/5482320?u=c2d4799ecd5e827076eca00cbd635b2720013685&v=4",
-    Hireable: true,
-    Name: "David R Soller",
-    repos: 36,
-    starredTime: "2017-01-20 06:26:19",
-    Username: "3ygun",
-    recommendations: 3.89010989010989,
-  },
-  {
-    email: "linyiyu1992@gmail.com",
-    avatar: "https://avatars.githubusercontent.com/u/3176481?v=4",
-    Hireable: true,
-    Name: "Yiyu Lin",
-    repos: 36,
-    starredTime: "2017-01-22 08:30:39",
-    Username: "attila-lin",
-    recommendations: 1.889010989010989,
-  },
-  {
-    email: "_@hideyukisaito.info",
-    avatar:
-      "https://avatars.githubusercontent.com/u/802921?u=e55932ec4f1df9f77bb34c25252e5fd39a407550&v=4",
-    Hireable: false,
-    Name: "Hideyuki Saito",
-    repos: 36,
-    starredTime: "2017-03-16 18:04:06",
-    Username: "hideyukisaito",
-    recommendations: 6.89010989010989,
-  },
-  {
-    email: "lg342@cornell.edu",
-    avatar:
-      "https://avatars.githubusercontent.com/u/62580?u=45ada638ed6b28729624313ecfac10b1c76fad4c&v=4",
-    Hireable: true,
-    Name: "Lee Gao",
-    repos: 35,
-    starredTime: "2017-01-18 19:33:19",
-    Username: "leegao",
-    recommendations: 9.615384615384617,
-  },
-];
+import { DUMMY_DATA } from "../data";
 
 function RecommendedProfilesPage() {
   const detailsCtx = useContext(DetailsContext);
@@ -65,37 +20,35 @@ function RecommendedProfilesPage() {
 
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("http://localhost:5001/recommend?username=deshraj")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const allUsers = [];
-        const individualUser = data.recommended_users;
-        for (const key in individualUser) {
-          console.log(individualUser[key]);
-          const individualUser2 = {
-            id: key,
-            ...individualUser[key],
-          };
-          allUsers.push(individualUser2);
-        }
-        setIsLoading(false);
-        setRecommendedProfiles(allUsers);
-        console.log("--------------------------");
-        console.log(allUsers);
-      });
-  }, []);
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("http://localhost:5001/recommend?username=deshraj")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const allUsers = [];
+  //       const individualUser = data.recommended_users;
+  //       for (const key in individualUser) {
+  //         console.log(individualUser[key]);
+  //         const individualUser2 = {
+  //           id: key,
+  //           ...individualUser[key],
+  //         };
+  //         allUsers.push(individualUser2);
+  //       }
+  //       setIsLoading(false);
+  //       setRecommendedProfiles(allUsers);
+  //     });
+  // }, []);
 
-  if (isLoading) {
-    return (
-      <section>
-        <p>loading...</p>
-      </section>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <section>
+  //       <p>loading...</p>
+  //     </section>
+  //   );
+  // }
 
   return (
     <div className={classes.basic}>
@@ -104,7 +57,7 @@ function RecommendedProfilesPage() {
         <Card>
           <Filter
             setFilteredProfiles={setFilteredProfiles}
-            allProfiles={recommendedProfiles}
+            allProfiles={DUMMY_DATA}
           />
         </Card>
       </div>

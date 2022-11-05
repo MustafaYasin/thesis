e200f8762ea78aa4da7d4599f0dd7391dafd43a7
rhofmann <r.hofmann2711@gmail.com>
2022-11-05 12:19:47 +0100

    building range slider

---
+++ project/frontend/src/components/filter/RangeSlider.js
@@ -0,0 +1,46 @@
+import classes from "./RangeSlider.module.css";
+
+function RangeSlider() {
+  return (
+    <div className={classes.rangeSlider}>
+      <div className={classes.wrapper}>
+        <header>
+          <h2>Repo Range</h2>
+          <p>Use slider or enter min and max Repo count</p>
+        </header>
+        <div className={classes.priceInput}>
+          <div className={classes.field}>
+            <span>Min</span>
+            <input type="number" name="input-min" id="input-min" value="25" />
+          </div>
+          <div className={classes.seperator}>-</div>
+          <div className={classes.field}>
+            <span>Max</span>
+            <input type="number" name="input-max" id="input-max" value="75" />
+          </div>
+        </div>
+        <div className={classes.slider}>
+          <div className={classes.progress}></div>
+        </div>
+        <div className={classes.rangeInput}>
+          <input
+            type="range"
+            className={classes.rangeMin}
+            min={0}
+            max={100}
+            value={25}
+          />
+          <input
+            type="range"
+            className={classes.rangeMax}
+            min={0}
+            max={100}
+            value={75}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RangeSlider;

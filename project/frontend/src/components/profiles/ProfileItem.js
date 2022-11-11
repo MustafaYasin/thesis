c7477a999a5c081f7c1165df88cac2417215ff1b
rhofmann <r.hofmann2711@gmail.com>
2022-11-11 11:13:29 +0100

    styled detailView

--- project/frontend/src/components/profiles/ProfileItem.js
+++ project/frontend/src/components/profiles/ProfileItem.js
@@ -2,6 +2,7 @@ import classes from "./ProfileItem.module.css";
 import Card from "../ui/Card";
 import { useContext } from "react";
 import FavoritesContext from "../store/favorites-context";
+import HorizontalBarChart from "../ui/HorizontalBarChart";
 
 function ProfileItem(props) {
   const favoritesCtx = useContext(FavoritesContext);
@@ -24,6 +25,9 @@ function ProfileItem(props) {
         company: props.company,
         primary_language: props.primary_language,
         yearsofExperience: props.yearsofExperience,
+        feature_1: props.feature_1,
+        feature_2: props.feature_2,
+        feature_3: props.feature_3,
       });
     }
   }
@@ -31,35 +35,70 @@ function ProfileItem(props) {
   return (
     <li className={classes.item}>
       <Card>
-        <div className={classes.image}>
-          <img src={props.avatar_url} alt={props.fullName} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.fullName}</h3>
-          <address>{props.email}</address>
-          <p>
-            {props.fullName} arbeitet bei{" "}
-            {props.company ? props.company : "Keine Angabe"},
-          </p>
-          <p>und hat {props.repository_count} Repos auf GitHub</p>
-          <p>
-            kurze Bio: {props.bio ? props.bio : "wurde leider nicht angegeben"}
-          </p>
-          <div className={classes.skills}>
-            <p>Skills:</p>
-            <div className={classes.programmingLanguageContainer}>
-              {Object.keys(props.primary_language).map((language) => (
-                <div key={language} className={classes.programmingLanguage}>
-                  {language}
+        <div className={classes.detailsViewContainer}>
+          <div className={classes.image}>
+            <img src={props.avatar_url} alt={props.fullName} />
+          </div>
+          <div className={classes.content}>
+            <div className={classes.contentHeader}>
+              <h3>{props.fullName}</h3>
+            </div>
+            <div className={classes.contentAddress}>
+              <address>{props.email}</address>
+            </div>
+            <div className={classes.contentInfoContainer}>
+              <div className={classes.contentInfoGrid}>
+                <div className={classes.leftGridContainer}>
+                  <div className={classes.barChartContainer}>
+                    <HorizontalBarChart
+                      feature_1={props.feature_1 * 100}
+                      feature_2={props.feature_2 * 100}
+                      feature_3={props.feature_3 * 100}
+                    />
+                  </div>
+                  <div className={classes.totalScoreContainer}>
+                    Results in Total Score of{" "}
+                    <p className={classes.totalScore}>
+                      {(
+                        ((props.feature_1 + props.feature_2 + props.feature_3) /
+                          3) *
+                        100
+                      ).toFixed(0)}
+                      /100
+                    </p>
+                  </div>
                 </div>
-              ))}
+                <div className={classes.aboutContainer}>
+                  <h2>About</h2>
+                  <div className={classes.bioContainer}>{props.bio}</div>
+                  <div className={classes.workingContainer}>
+                    currently working at{" "}
+                    <span className={classes.company}>{props.company}</span>{" "}
+                    with{" "}
+                    <span className={classes.yearsofExperience}>
+                      {props.yearsofExperience} years
+                    </span>{" "}
+                    of Experience.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={classes.skills}>
+              <h2>Skills:</h2>
+              <div className={classes.programmingLanguageContainer}>
+                {Object.keys(props.primary_language).map((language) => (
+                  <div key={language} className={classes.programmingLanguage}>
+                    {language}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className={classes.actions}>
-          <button onClick={toggleFavoriteStatusHandler}>
-            {itemIsFavorite ? "Remove from Favorites" : "To Favorites"}
-          </button>
+          <div className={classes.actions}>
+            <button onClick={toggleFavoriteStatusHandler}>
+              {itemIsFavorite ? "Remove from Favorites" : "To Favorites"}
+            </button>
+          </div>
         </div>
       </Card>
     </li>

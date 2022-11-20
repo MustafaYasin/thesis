c7277ede6000b18cf31bfb6d45852588cf1afc4a
Mustafa Yasin <myasin@adobe.com>
2022-11-20 23:19:59 +0100

    added markdown to text function to parse only text

--- project/backend/recommender_system/readme_2.py
+++ project/backend/recommender_system/readme_2.py
@@ -1,19 +1,31 @@
 import wget
+from markdown import markdown
+from bs4 import BeautifulSoup
+import re
 
-
-# wget https://raw.githubusercontent.com/{owner}/{repo}/{branch}/README.md
+# # wget https://raw.githubusercontent.com/{owner}/{repo}/{branch}/README.md
 URL = "https://raw.githubusercontent.com"
 OWNER = 'MustafaYasin'
 REPO = "MustafaYasin"
 BRANCH = "main" 
 
-def get_readme(URL, OWNER, REPO, BRANCH):
+def get_readme():
     url = URL +'/' + OWNER + '/' + REPO + '/' + BRANCH + '/' + "README.md"
-    readmeMarkdown = wget.download(url)
-    readme = readmeMarkdown.to_string()
+    readme = wget.download(url, out="../user_data_csv/README.text", )
     return readme
 
-print(get_readme(URL, OWNER, REPO, BRANCH))
+def markdown_to_text():
+    readme = get_readme()
+    html = markdown(readme)
+    text = ''.join(BeautifulSoup(html, features="lxml").findAll(text=True))
+    return text
+
+
+print(markdown_to_text())
+
+
+
+
 
 
 

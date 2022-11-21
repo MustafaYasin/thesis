de63311391a036933176be947af71df392042b1e
Mustafa Yasin <myasin@adobe.com>
2022-11-21 11:34:26 +0100

    fixed the markdown to text issue

--- project/backend/recommender_system/readme.py
+++ project/backend/recommender_system/readme.py
@@ -9,11 +9,20 @@ OWNER = 'MustafaYasin'
 REPO = "MustafaYasin"
 BRANCH = "main" 
 
+# Function to download the README.md file from each repository
 def get_readme():
     url = URL +'/' + OWNER + '/' + REPO + '/' + BRANCH + '/' + "README.md"
     readme = wget.download(url, out="../user_data_csv/README.md", )
-    # html = markdown('../user_data_csv/README.md')
-    # text = ''.join(BeautifulSoup(html, features="lxml").findAll(text=True))
-    # print (text)
-    return readme
+
+    # Open the downloaded Markdown file and read it into a variable
+    # After that, convert the Markdown to HTML
+    with open(readme, 'r') as f:
+        text = f.read()
+        print(text)
+        html = markdown(text)
+        text = ''.join(BeautifulSoup(html, features="lxml").findAll(text=True))
+        print (text)
+    return text
+
+text = get_readme()
 

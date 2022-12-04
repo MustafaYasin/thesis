53e5ec99bd8467f4d6c63e8d5a6d764fb5976c72
rhofmann <r.hofmann2711@gmail.com>
2022-12-04 13:22:31 +0100

    adding Coming Soons for Homepage

--- project/frontend/src/pages/HomePage.js
+++ project/frontend/src/pages/HomePage.js
@@ -10,26 +10,73 @@ function CompareFavoritesPage() {
       <div className={classes.homePageHeaderContainer}>
         <h1>HomePage</h1>
       </div>
-      <div className={classes.cardContainer}>
-        <Card>
-          <div className={classes.cardContentContainer}>
-            <div className={classes.cardContentHeader}>
-              <h2>Looking for Data Scientist?</h2>
-            </div>
-            <div className={classes.imageContainer}>
-              <img
-                className={classes.image}
-                src="https://www.sudeep.co/images/post_images/2018-02-09-Understanding-the-Data-Science-Lifecycle/chart.png"
-                alt=""
-              />
-              <div className={classes.link}>
-                <Link to="/recommend">
-                  <button> Click Here</button>
-                </Link>
+
+      <div className={classes.categoryContainer}>
+        <div className={classes.cardContainer}>
+          <Card>
+            <div className={classes.cardContentContainer}>
+              <div className={classes.cardContentHeader}>
+                <h2>Looking for Data Scientist?</h2>
+              </div>
+              <div className={classes.imageContainer}>
+                <img
+                  className={classes.image}
+                  src="https://www.sudeep.co/images/post_images/2018-02-09-Understanding-the-Data-Science-Lifecycle/chart.png"
+                  alt=""
+                />
+                <div className={classes.link}>
+                  <Link to="/recommend">
+                    <button> Click Here</button>
+                  </Link>
+                </div>
               </div>
             </div>
+          </Card>
+        </div>
+
+        <div className={classes.cardContainer}>
+          <div className={classes.comingSoon}>
+            <Card>
+              <div className={classes.cardContentContainer}>
+                <div className={classes.cardContentHeader}>
+                  <h2>Looking for Software Engineer?</h2>
+                </div>
+                <div className={classes.imageContainer}>
+                  <img
+                    className={classes.image}
+                    src="https://w7.pngwing.com/pngs/386/188/png-transparent-programmer-programming-language-software-developer-computer-programming-sublime-text-computer-cartoon-furniture-reading-cartoon.png"
+                    alt=""
+                  />
+                  <div className={classes.comingSoonButton}>
+                    <button> COMING SOON! </button>
+                  </div>
+                </div>
+              </div>
+            </Card>
           </div>
-        </Card>
+        </div>
+
+        <div className={classes.cardContainer}>
+          <div className={classes.comingSoon}>
+            <Card>
+              <div className={classes.cardContentContainer}>
+                <div className={classes.cardContentHeader}>
+                  <h2>Looking for UX/UI design?</h2>
+                </div>
+                <div className={classes.imageContainer}>
+                  <img
+                    className={classes.image}
+                    src="https://miro.medium.com/max/1100/1*VsNDNsrY96XmmBEpftI2Hw.webp"
+                    alt=""
+                  />
+                  <div className={classes.comingSoonButton}>
+                    <button> COMING SOON!</button>
+                  </div>
+                </div>
+              </div>
+            </Card>
+          </div>
+        </div>
       </div>
     </>
   );

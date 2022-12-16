99d27740ba5f6153eaebe6254bb72e8cb4f8b755
rhofmann <r.hofmann2711@gmail.com>
2022-12-16 17:19:26 +0100

    random fe slider or radar

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -7,12 +7,13 @@ import HomePage from "./pages/HomePage";
 import RecommendedProfilesPage2 from "./pages/RecommendedProfilesPage2";
 
 function App() {
+  var random_boolean = Math.random() < 0.5;
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/recommend" element={<RecommendedProfilesPage />}></Route>
-        <Route path="/recommend2" element={<RecommendedProfilesPage2 />}></Route>
+        {random_boolean?<Route path="/recommend" element={<RecommendedProfilesPage />}></Route>:
+        <Route path="/recommend" element={<RecommendedProfilesPage2 />}></Route>}
         {/* <Route path="/compare" element={<CompareFavoritesPage />}></Route> */}
         <Route path="/favorites" element={<FavoritesPage />}></Route>
       </Routes>
